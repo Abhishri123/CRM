864bf3ea953b53aec6fcee8597b3540338cb605f
Abhishri123 <vithalaniabhishree@gmail.com>
2022-09-22 22:55:30 +0530

    Changes

--- crm_company_employee/resources/views/admin/includes/footer.blade.php
+++
@@ -1,7 +0,0 @@
-  <!-- partial:partials/_footer.html -->
-  <footer class="footer">
-          <div class="container-fluid clearfix">
-            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2022. Made By Abhishri Vithalani</span>
-          </div>
-        </footer>
-        <!-- partial -->
